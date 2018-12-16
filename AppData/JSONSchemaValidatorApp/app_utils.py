7d58a2f1310fc4684474cc92c0d40470ad667b01
Odyseus <venusyadonis@gmail.com>
2018-12-16 14:11:42 -0300

    app_utils.py
    - Improvements of the kind "do more with less code".
    - Improved logging.
    - Improved exception catching.
    - Added the possibility to specify nested properties for JSON files, not just Python files.

--- AppData/JSONSchemaValidatorApp/app_utils.py
+++ AppData/JSONSchemaValidatorApp/app_utils.py
@@ -13,16 +13,23 @@ import os
 
 from runpy import run_path
 
-from .python_utils import exceptions
 from .python_utils import file_utils
 from .python_utils import json_schema_utils
+from .python_utils.exceptions import ExceptionWhitoutTraceBack
 
 
 root_folder = os.path.realpath(os.path.abspath(os.path.join(
     os.path.normpath(os.getcwd()))))
 
 
-class InvalidFile(exceptions.ExceptionWhitoutTraceBack):
+class NonExistentFile(ExceptionWhitoutTraceBack):
+    """NonExistentFile"""
+
+    def __init__(self, msg):
+        super().__init__(msg)
+
+
+class InvalidFile(ExceptionWhitoutTraceBack):
     """InvalidFile"""
 
     def __init__(self, file_path):
@@ -31,62 +38,92 @@ class InvalidFile(exceptions.ExceptionWhitoutTraceBack):
         super().__init__(msg)
 
 
-def get_value_from_object(obj, props):
-    props = props.split(".")
-    desired_value = obj[props.pop(0)]
+class MissingRequiredParameter(ExceptionWhitoutTraceBack):
+    """MissingRequiredParameter"""
 
-    while props:
-        desired_value = desired_value[props.pop(0)]
+    def __init__(self, missing_parameter, file_path):
+        msg = "It is mandatory to specify a property to get data from a Python file.\n"
+        msg += "**Missing parameter: %s\n**" % missing_parameter
+        msg += "**File were the property should exist:\n%s**" % file_path
+        super().__init__(msg)
+
+
+def get_value_from_object(file_path, obj, props, logger):
+    if isinstance(props, str):
+        props = props.split(".")
+    else:
+        return obj
+
+    try:
+        desired_value = obj[props.pop(0)]
+
+        while props:
+            desired_value = desired_value[props.pop(0)]
+    except KeyError as err:
+        msg = "**KeyError:** %s\n" % str(err)
+        msg += "**File path:** %s" % file_path
+        logger.error(msg)
+        raise SystemExit(1)
 
     return desired_value
 
 
 def validate_schema(data_file, schema_file,
-                    data_prop="data",
-                    schema_prop="schema",
+                    data_prop=None,
+                    schema_prop=None,
                     logger=None):
-    data_file = os.path.realpath(os.path.abspath(os.path.join(
-        os.path.normpath(data_file))))
-    schema_file = os.path.realpath(os.path.abspath(os.path.join(
-        os.path.normpath(schema_file))))
+    storage = {
+        "instance": {
+            "file": os.path.realpath(os.path.abspath(os.path.join(
+                os.path.normpath(file_utils.expand_path(data_file))))),
+            "data": None,
+            "property": data_prop,
+            "extension": os.path.splitext(data_file)[1].lower()
+        },
+        "schema": {
+            "file": os.path.realpath(os.path.abspath(os.path.join(
+                os.path.normpath(file_utils.expand_path(schema_file))))),
+            "data": None,
+            "property": schema_prop,
+            "extension": os.path.splitext(schema_file)[1].lower()
+        }
+    }
 
-    for f_path in [data_file, schema_file]:
-        if not file_utils.is_real_file(f_path):
-            raise exceptions.MissingRequiredFile("The following file doesn't exists: %s\n" %
-                                                 f_path)
-
-    data_file_extension = os.path.splitext(data_file)[1].lower()
-    schema_file_extension = os.path.splitext(schema_file)[1].lower()
+    for x in storage.values():
+        if not file_utils.is_real_file(x["file"]):
+            raise NonExistentFile("The following file doesn't exists: %s\n" %
+                                  x["file"])
 
     try:
-        if data_file_extension == ".json":
-            instance = json.load(data_file)
-        elif data_file_extension == ".py":
-            instance = get_value_from_object(run_path(data_file), data_prop)
-        else:
-            raise InvalidFile(data_file)
+        for k, v in storage.items():
+            if v["extension"] == ".json":
+                with open(v["file"], "r") as file_data:
+                    v["data"] = get_value_from_object(v["file"], json.load(
+                        file_data), v["property"], logger)
+            elif v["extension"] == ".py":
+                if v["property"] is None:
+                    raise MissingRequiredParameter("--%s-prop" % k, v["file"])
 
-        if schema_file_extension == ".json":
-            schema = json.load(schema_file)
-        elif schema_file_extension == ".py":
-            schema = get_value_from_object(run_path(schema_file), schema_prop)
-        else:
-            raise InvalidFile(schema_file)
-
+                v["data"] = get_value_from_object(
+                    v["file"], run_path(v["file"]), v["property"], logger)
+            else:
+                raise InvalidFile(v["file"])
     except Exception as err:
         logger.error(err)
         raise SystemExit(1)
 
-    json_schema_utils.validate(instance, schema,
-                               error_message_extra_info="\n".join([
-                                   "Data file: %s" % data_file,
-                                   "" if data_file_extension == ".py" else "Data key: %s" % data_prop,
-                                   "Schema file: %s" % schema_file,
-                                   "" if schema_file_extension == ".py" else "Schema key: %s" % schema_prop
-                               ]),
+    extra_info = "\n".join([
+        "**Data file:** %s" % storage["instance"]["file"],
+        "**Data key:** %s" % str(storage["instance"]["property"]),
+        "**Schema file:** %s" % storage["schema"]["file"],
+        "**Schema key:** %s" % str(storage["schema"]["property"])
+    ])
+
+    json_schema_utils.validate(storage["instance"]["data"], storage["schema"]["data"],
+                               error_message_extra_info=extra_info,
                                logger=logger)
 
-    logger.success("No validation errors found!!!")
+    logger.success("**No validation errors found!!!**\n" + extra_info)
 
 
 if __name__ == "__main__":
