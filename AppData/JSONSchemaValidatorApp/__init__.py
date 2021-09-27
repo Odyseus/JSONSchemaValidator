971b1ff87105cfab33709946abf266a011fd9fd1
Odyseus <venusyadonis@gmail.com>
2021-09-27 08:42:42 -0300

    Updated manual page and bump version

--- AppData/JSONSchemaValidatorApp/__init__.py
+++ AppData/JSONSchemaValidatorApp/__init__.py
@@ -3,7 +3,7 @@
 __status__ = "Unstable"
 __appname__ = "JSON Schema Validator"
 __appdescription__ = "Utility to validate data from JSON schemas."
-__version__ = "2020-03-20 21:47:57.128"
+__version__ = "2021-09-27 08:34:09.369"
 
 if __name__ == "__main__":
     pass
